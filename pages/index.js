a4a5efa4eabcecb5a963f06e5d760bb16cbf117c
HassanKhan123 <hk2602809@gmail.com>
2021-02-17 22:30:41 +0500

    linking between pages added

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
@@ -17,6 +18,10 @@ export default function Home() {
         Magna reprehenderit reprehenderit laborum occaecat adipisicing duis
         consequat laborum commodo deserunt ea do consectetur.
       </p>
+
+      <Link href='/ninjas'>
+        <a>See all ninjas</a>
+      </Link>
       <Footer />
     </div>
   );
