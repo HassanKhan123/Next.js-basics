93ffe5fec17cf5c591bad5a248e35d44f26a845f
HassanKhan123 <hk2602809@gmail.com>
2021-02-17 22:38:38 +0500

    layout component added

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
 import Head from 'next/head';
 import Link from 'next/link';
 
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <p>
         Magna reprehenderit reprehenderit laborum occaecat adipisicing duis
@@ -22,7 +19,6 @@ export default function Home() {
       <Link href='/ninjas'>
         <a>See all ninjas</a>
       </Link>
-      <Footer />
     </div>
   );
 }
