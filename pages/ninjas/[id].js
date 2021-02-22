5a4b04fe0a5f6a44ac99331dbe97d44b0a5d69ac
HassanKhan123 <hk2602809@gmail.com>
2021-02-22 11:59:15 +0500

    getStaticPaths method added

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,5 +1,23 @@
 import React from 'react';
 
+export const getStaticPaths = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  const paths = data.map((ninja) => {
+    return {
+      params: {
+        id: ninja.id.toString(),
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
 function Details() {
   return (
     <div>
